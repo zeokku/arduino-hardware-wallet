bfc1d5930af1d3bac491fe6b0ea8805d7f9f43df
Luty <r89534217610@gmail.com>
2022-02-14 23:44:10 +0300

    almost done

--- Crypto-Wallet.ino
+++ Crypto-Wallet.ino
@@ -1,18 +1,20 @@
 #include <EEPROM.h>
 #include <AES.h>
 #include <OFB.h>
-#include <BLAKE2s.h>
+#include <Ed25519.h>
+#include <SHA512.h>
 
+#pragma region utils
 const int ledPin = 11;
 
-
-void print_bytes(const byte* buf, byte len) {
-  for (byte i = 0; i < len; i += 1) {
+void print_bytes(byte* buf, uint16_t len) {
+  byte* buf_end = buf + len;
+  for (; buf < buf_end; buf += 1) {
     // bruh this shit doesn't print bytes with padded leading 0
     //Serial.print(buf[i], HEX);
 
-    byte b0 = buf[i] >> 4;
-    byte b1 = buf[i] & 0xF;
+    byte b0 = *buf >> 4;
+    byte b1 = *buf & 0xF;
 
     Serial.print(b0, HEX);
     Serial.print(b1, HEX);
@@ -23,16 +25,18 @@ void print_bytes(const byte* buf, byte len) {
 }
 
 /// For a Teensy 2.0, Address can be 0 to 1023
-void read_bytes_mem(byte* buf, byte len, uint16_t addr) {
+void read_bytes_mem(byte* buf, uint16_t len, uint16_t addr) {
   for (byte i = 0; i < len; i += 1, addr += 1) {
     buf[i] = EEPROM.read(addr);
   }
 }
-void write_bytes_mem(const byte* buf, byte len, uint16_t addr) {
+void write_bytes_mem(const byte* buf, uint16_t len, uint16_t addr) {
   for (byte i = 0; i < len; i += 1, addr += 1) {
     EEPROM.write(addr, buf[i]);
   }
 }
+
+#pragma endregion
 
 // pepper to be used as salt. why pepper? because it's constantantly defined
 // generated from random.org
@@ -44,71 +48,122 @@ const byte pepper[] = {
   25,  11,  189, 16,
 };
 
+
+#pragma region func declarations
+
 uint32_t generate_seed();
-void hash();
-void store_key();
+
+void store_key(byte*, char*);
+void generate_key(char*);
+
+#pragma endregion
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  Serial.printlnuint32_t(-1));
-
   uint32_t seed = generate_seed();
   randomSeed(seed);
 
-  //Serial.print("seed: ");
-  //Serial.println(seed);
+  ///////////
 
-  
+  byte* input_key = new byte[32];
+  memset(input_key, 0x10, 32);
 
-  //229
-  //187 160 94 171 133 68 8 20 175 243 44 17 223 252 83 43
-  //Serial.println(random(256));
+  char* password = new char[256];
+  memset(password, 0, 256);
+  memcpy(password, "👽", 4);
 
-  //byte* iv = new byte[16];
-  //random_bytes(iv, 16);
+  //store_key(input_key, password);
+  generate_key(password);
 
-  //no idea why but stupid ass serial js lib couldn't properly read shit from here bruh
-
-  //store_key();
+  //sign_payload("🍆")
 }
 
-void hash() {
+void loop() {
+  // read is ended with 0x0a (10) = new line
+  if (Serial.available() > 0) {
+    //Serial.println(Serial.readString());
+    byte operation = Serial.read();
+    //Serial.print("USB received: ");
+    //Serial.println(incomingByte, DEC);
 
-  char password[] = "test string";
-  int password_len = sizeof(password) - 1; // sub 1 for \0
+    switch (operation) {
+      case 0x01:
+        //service op
+        break;
+      case 0x10:
+      case 'e':
+        {
+          //read key and pass here
+
+          //store_key(input_key, password);
+        }
+        break;
+      case 'g'://generate key
+        {
+          char* password = new char[256];
+          memset(password, 0, 256);
+          memcpy(password, "test", 4);
+
+          generate_key(password);
+        }
+        break;
+      default:
+        Serial.write(operation);
+    }
+
+    //Discard any received data that has not been read
+    //Serial1.clear()
+
+  }
+}
+
+void zkdf(char* password, byte* result) {
+  uint16_t password_len = strlen(password);
 
   Serial.println(password);
   Serial.println(password_len);
 
-  byte payload_len = password_len + sizeof(pepper);
+  uint16_t payload_len = password_len + sizeof(pepper);
   byte* payload = new byte[payload_len];
+
   memcpy(payload, password, password_len);
   memcpy(payload + password_len, pepper, sizeof(pepper));
 
-  //print_bytes((byte*)password, password_len);
-  //print_bytes(pepper, sizeof(pepper));
-  //print_bytes(payload, password_len + sizeof(pepper));
+  //rip sha3
+  //SHA3_256* hash = new SHA3_256();
 
-  BLAKE2s* blake = new BLAKE2s();
+  //so this is essentially SHA512/256
+  SHA512* hash = new SHA512();
 
-  byte digest_len = blake->hashSize();
-  byte* digest = new byte[digest_len];
+  hash->update(payload, payload_len);
+  //nice we can truncate right in finalize()
+  hash->finalize(result, 32);
 
-  blake->update(payload, payload_len);
-  blake->finalize(digest, digest_len);
+  delete hash;
 
-  delete blake;
 
-  Serial.println(digest_len);
-  print_bytes(digest, digest_len);
+  /*
+    BLAKE2s* blake = new BLAKE2s();
 
-  //pinMode(ledPin, OUTPUT);
-  //digitalWrite(ledPin, HIGH); // LOW = turn off
-  //delay(1000); // ms
+    byte digest_len = blake->hashSize();
+    byte* digest = new byte[digest_len];
 
-  Serial.write(0x01);
+    blake->update(payload, payload_len);
+    blake->finalize(digest, digest_len);
+
+    delete blake;
+
+    Serial.println(digest_len);
+    print_bytes(digest, digest_len);
+
+    //pinMode(ledPin, OUTPUT);
+    //digitalWrite(ledPin, HIGH); // LOW = turn off
+    //delay(1000); // ms
+
+    Serial.write(0x01);
+  */
 
 }
 
@@ -136,94 +191,102 @@ uint32_t generate_seed() {
   return (long_seed >> 32) ^ (long_seed & uint32_t(-1));
 }
 
-void random_bytes(const byte* buf, byte len) {
+void random_bytes(byte* buf, byte len) {
   for (byte b = 0; b < len; b += 1) {
     byte r = random();
-    
+
     buf[b] = r;
-    
+
     //Serial.print(r);
     //Serial.print(' ');
   }
 }
 
+/// clears input_key, password buffers after finished
+/// password is \0 terminated string
+void store_key(byte private_key[32], char* password) {
+  
+  Serial.print("Password bytes: ");
+  print_bytes((byte*)password, 32);//it's actually 256 buf
+  Serial.print("Password: ");
+  Serial.println(password);
+  
+  byte* encryption_key = new byte[32];
 
-void store_key() {
-  byte* input_key = new byte[32];
-  memset(input_key, 0x00, 32);
-  byte* password = new byte[32];
-  memset(password, 0x00, 32);
-  password[0] = 0x80;
-
-  byte* encrypted = new byte[32];
+  zkdf(password, encryption_key);
+  memset(password, 0, 256);
 
   byte* iv = new byte[16];
   random_bytes(iv, 16);
 
+  byte* encrypted = new byte[32];
+
   // BLOCK SIZE IS 128 BIT
   OFB<AES256>* aes = new OFB<AES256>();
   //AES256* aes = new AES256();
-  aes->setKey(password, 32);
+  aes->setKey(encryption_key, 32);
   aes->setIV(iv, 16);
 
-  aes->encrypt(encrypted, input_key, 32);
-
-  //    aes->encryptBlock(encrypted, input_key);
-  //    aes->encryptBlock(encrypted + 16, input_key + 16);
-
-  Serial.write(0x10);
-
-  Serial.write(encrypted, 32);
-  Serial.write(input_key, 32);
-  Serial.write(password, 32);
-
-  Serial.println();
-
-  print_bytes(iv, 16);
-  print_bytes(encrypted, 32);
-  print_bytes(input_key, 32);
-  print_bytes(password, 32);
+  aes->encrypt(encrypted, private_key, 32);
 
   delete aes;
 
+  //Serial.write(0x10);
+  //Serial.println();
+
+  Serial.print("Private key: ");
+  print_bytes(private_key, 32);
+
+  Serial.print("IV: ");
+  print_bytes(iv, 16);
+
+  Serial.print("Encrypted: ");
+  print_bytes(encrypted, 32);
+
+#pragma region decryption test
+
+  aes = new OFB<AES256>();
+  aes->setKey(encryption_key, 32);
+  aes->setIV(iv, 16);
+
+  byte* decrypted = new byte[32];
+  aes->decrypt(decrypted, encrypted, 32);
+
+  delete aes;
+
+  Serial.print("Decrypted: ");
+  print_bytes(decrypted, 32);
+  delete[] decrypted;
+
+#pragma endregion
+
   //clear key buffer with overwriting data
   //crypto lib automatically overwrites own internal buffers
-  memset(input_key, 0, 32);
-  memset(password, 0, 32);
+  memset(private_key, 0, 32);
+  memset(encryption_key, 0, 32);
 
+  //@todo
+  //write_bytes_mem(encrypted, 32, 0);
 
-  delete[] input_key;
   delete[] password;
   delete[] encrypted;
   delete[] iv;
 }
 
-void loop() {
-  // read is ended with 0x0a (10) = new line
-  if (Serial.available() > 0) {
-    //Serial.println(Serial.readString());
-    byte operation = Serial.read();
-    //Serial.print("USB received: ");
-    //Serial.println(incomingByte, DEC);
+void sign_payload(byte private_key[32], byte* payload, uint16_t len){
+  byte signature[64];
+  Ed25519::sign(signature, private_key, private_key, payload, len);
+}
 
-    switch (operation) {
-      case 0x01:
-        hash();
-        break;
-      case 0x10:
-      case 'e':
-        store_key();
-        break;
-      default:
-        Serial.write(operation);
-    }
+void generate_key(char* password) {
+  byte* private_key = new byte[32];
+  byte* public_key = new byte[32];
 
-    //Discard any received data that has not been read
-    //Serial1.clear()
+  Ed25519::generatePrivateKey(private_key);
+  Ed25519::derivePublicKey(public_key, public_key);
 
-  }
+  Serial.print("Public key: ");
+  print_bytes(public_key, 32);
 
-
-
-
+  store_key(private_key, password);
 }
--- Operations.h
+++ Operations.h
@@ -1,3 +1,16 @@
 #define OP_HASH = 0x01;
 
 #define OP_STORE_KEY = 0x10;
+
+/*
+it needs to:
+ store key,
+ generate signature,
+ export key,
+ 
+ generate key 
+*/
+
+/*
+gen key -> encrypt, store
+*/
